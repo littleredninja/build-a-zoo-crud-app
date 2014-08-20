b7b8beb8e1e9f462913abcab9f29cb0327057982
Jennie Chamberlin <chamberlin.jennie@gmail.com>
2014-08-20 11:09:02 -0500

    added edit zoo, add, edit and delete animals

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,7 +9,8 @@ post '/sessions' do
 			session[:user_id] = @user.id
 			redirect ('/zoos')
 		else
-			redirect ('/')
+			@error = "So sad, can't sign in.  Please try again."
+			erb :index
 		end
 end
 
@@ -38,3 +39,61 @@ post '/zoos/new' do
 	redirect ('/zoos')
 end
 
+get '/zoos/:zoo_id/edit' do
+	@zoo = Zoo.find(params[:zoo_id])
+	erb :edit_zoo
+end
+
+put '/zoos/:zoo_id/edit' do
+	@zoo = Zoo.find(params[:zoo_id])
+	@zoo.update(params[:zoo])
+	redirect ('/zoos')
+end
+
+delete '/zoos/:zoo_id' do
+	@zoo = Zoo.find(params[:zoo_id])
+	@zoo.destroy
+	redirect('/zoos')
+end
+
+get '/zoos/:zoo_id/animals/new' do
+	@zoo = Zoo.find(params[:zoo_id])
+	erb :"zoo_animals_add"
+end
+
+get '/zoos/:zoo_id/animals' do
+	@zoo = Zoo.find(params[:zoo_id])
+	@animals = @zoo.animals
+	erb :zoo_animals
+end
+
+post '/zoos/:zoo_id/animals' do
+	@zoo = Zoo.find(params[:zoo_id])
+	new_animal = Animal.create(params[:animal])
+	new_animal.update(zoo: @zoo)
+	redirect ("/zoos/#{@zoo.id}/animals")
+end
+
+get '/animal/:animal_id/edit' do
+	@animal = Animal.find(params[:animal_id])
+	erb :zoo_animals_edit
+end
+
+put '/animal/:animal_id/edit' do
+	@animal = Animal.find(params[:animal_id])
+	@zoo = @animal.zoo
+	@animal.update(params[:animal])
+	redirect ("/zoos/#{@zoo.id}/animals")
+end
+
+delete '/animal/:animal_id/delete' do
+	@animal = Animal.find(params[:animal_id])
+	@zoo = @animal.zoo
+	@animal.destroy
+	redirect ("/zoos/#{@zoo.id}/animals") 
+end
+
+get "/logout" do
+	session[:user_id] =  nil 
+	erb :index
+end
